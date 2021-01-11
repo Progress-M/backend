afbbe5fff787f7df5e7fe7d8d92b7d412659076b
Dedok3d <sersh_96@mail.ru>
2021-01-11 23:48:46 +0700

    add UseKestrel

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace Main
@@ -14,7 +15,18 @@ namespace Main
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseKestrel(options =>
+                    {
+                        options.Limits.MaxConcurrentConnections = 100;
+                        options.Limits.MaxRequestBodySize = 10 * 1024;
+                        options.Limits.MinRequestBodyDataRate =
+                            new MinDataRate(bytesPerSecond: 100, gracePeriod: System.TimeSpan.FromSeconds(10));
+                        options.Limits.MinResponseDataRate =
+                            new MinDataRate(bytesPerSecond: 100, gracePeriod: System.TimeSpan.FromSeconds(10));
+                        options.Listen(System.Net.IPAddress.Loopback, 5003);
+                    })
+                    .UseStartup<Startup>();
                 });
     }
 }
