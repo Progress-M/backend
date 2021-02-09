9eb9d45c5922bf1603eed4331b225762c10d2640
Dedok3d <sersh_96@mail.ru>
2021-02-09 21:58:08 +0700

    fix: remove Kestrel

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace Main
@@ -16,16 +15,6 @@ namespace Main
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
-                    .UseKestrel(options =>
-                    {
-                        options.Limits.MaxConcurrentConnections = 100;
-                        options.Limits.MaxRequestBodySize = 10 * 1024;
-                        options.Limits.MinRequestBodyDataRate =
-                            new MinDataRate(bytesPerSecond: 100, gracePeriod: System.TimeSpan.FromSeconds(10));
-                        options.Limits.MinResponseDataRate =
-                            new MinDataRate(bytesPerSecond: 100, gracePeriod: System.TimeSpan.FromSeconds(10));
-                        options.Listen(System.Net.IPAddress.Loopback, 5003);
-                    })
                     .UseStartup<Startup>();
                 });
     }
